39296163abd935e1b85fdd0e2d07befeb4c6d2e7
羅中海 <luozhonghai@163.com>
2016-01-17 17:36:43 +0800

    implement leke api new

--- app/controllers/api/v1/leke/users_controller.rb
+++ app/controllers/api/v1/leke/users_controller.rb
@@ -1,16 +1,47 @@
 class Api::V1::Leke::UsersController < Lina::ApplicationController
+  rescue_from ::Exception, with: :error_occurred
+    
+  def error_occurred(e)
+    render json: { error: e.message}
+  end
+
   # def new
   define_action :new, {
     name: 'new',
     description: '',
     params: {
       type: 'object',
+      required: [ 'ip' ],
       properties: {
+        name: {
+          type: 'string',
+          description: 'ip地址',
+        }
       }
     },
     return: {
+      type: 'object',
+      required: [ 'code' ],
+      properties: {
+        code: {
+          type: 'string',
+          description: '成功返回1，失败返回0',
+        }
+      }
     }
   } do
     # write code here, It's the same as before
+    @user = Leke::User::find_by(ip: params[:ip])
+    if @user == nil
+      @user = Leke::User.create(ip: params[:ip])
+    end
+    @play = Leke::Play::create(user: @user.id)
+
+    if @play != nil  && @user != nil
+      render json: { code: "1" }
+    else
+      render json: { code: "0" }      
+    end
+
   end
 end
